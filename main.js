aacf31beed7e1295431d8972e0413f26a0cd6844
Jonas Stattin <jonas@vinnovera.se>
2023-01-10 11:42:52 +0100

    Sample usage of API

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 const API_BASE = 'https://nackademin-item-tracker.herokuapp.com/'
-//const API_BASE = 'http://localhost:3000/'
 
+const createListForm = document.querySelector('#create-list')
+const createListNameField = document.querySelector('#create-list-name')
 const listSearchForm = document.querySelector('#list-search')
 const listNameField = document.querySelector('#list-name')
 const listsHolder = document.querySelector('#lists')
 const listnameOutput = document.querySelector('#listname')
 const listItemHolder = document.querySelector('#item-list')
+const addItemForm = document.querySelector('#add-item')
+const itemTitleField = document.querySelector('#item-title')
+const itemDescField = document.querySelector('#item-desc')
 
-let currentList
+let currentList = ''
+
+function createItem(item) { 
+  const liElem = document.createElement('li')
+  liElem.innerHTML = `<h4>${item.title}</h4><p>${item.description}</p>`
+  const deleteItemBtn = document.createElement('button')
+  deleteItemBtn.innerText = 'Del'
+  liElem.appendChild(deleteItemBtn)
+  listItemHolder.appendChild(liElem)
+
+  deleteItemBtn.addEventListener('click', async function () { 
+    const res = await fetch(`${API_BASE}lists/${currentList}/items/${item._id}`, {
+      method: 'DELETE'
+    })
+    const { list } = await res.json()
+
+    drawItems(list.itemList)
+  })
+}
 
 function drawItems(items) {
   listItemHolder.innerHTML = ''
 
   items.forEach(item => { 
-    const liElem = document.createElement('li')
-    liElem.innerHTML = `<h4>${item.title}</h4><p>${item.description}</p>`
-    const deleteItemBtn = document.createElement('button')
-    deleteItemBtn.innerText = 'Del'
-    liElem.appendChild(deleteItemBtn)
-    listItemHolder.appendChild(liElem)
-
-    deleteItemBtn.addEventListener('click', function () { 
-      console.log(item)
-    })
+    createItem(item)
   })
 }
 
@@ -38,12 +51,33 @@ function drawLists(lists) {
     listsHolder.appendChild(liElem)
 
     chooseListBtn.addEventListener('click', function () { 
-      currentList = list.id
+      currentList = list._id
       listnameOutput.innerText = list.listname
       drawItems(list.itemList)
+       listsHolder.innerHTML = ''
     })
   })
 }
+
+createListForm.addEventListener('submit', async function (e) { 
+  e.preventDefault()
+
+  const listname = createListNameField.value
+  const res = await fetch(`${API_BASE}lists`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      listname: listname
+    })
+  })
+
+  const { list } = await res.json()
+  currentList = list._id
+  listnameOutput.innerText = list.listname
+  drawItems(list.itemList)
+})
 
 listSearchForm.addEventListener('submit', async function (e) { 
   e.preventDefault()
@@ -53,4 +87,33 @@ listSearchForm.addEventListener('submit', async function (e) {
   const data = await res.json()
 
   drawLists(data)
+})
+
+addItemForm.addEventListener('submit', async function (e) { 
+  e.preventDefault()
+
+  if (!currentList) { 
+    return alert('Choose a list')
+  }
+
+  const title = itemTitleField.value
+  const desc = itemDescField.value
+
+  if (!title || !desc) { 
+    return alert('Fill text boxes')
+  }
+
+  const res = await fetch(`${API_BASE}lists/${currentList}/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: title,
+      description: desc
+    })
+  })
+  const { list } = await res.json()
+
+  drawItems(list.itemList)
 })
